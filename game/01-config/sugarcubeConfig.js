31aecf88756a59822a12b75060d138d15fb0a2f5
AnonymousPossum <anonymousxpossum@gmail.com>
2024-06-19 12:27:28 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Jimmys <jimmysilverstein@proton.me>

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -7,7 +7,7 @@ Config.history.maxStates = 5;
 /* debug will enable or disable the feature only for new games */
 /* sneaky will enable the Sneaky notice banner on the opening screen and save display */
 /* versionName will be displayed in the top right of the screen, leave as "" to not display anything */
-window.StartConfig = {
+const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
