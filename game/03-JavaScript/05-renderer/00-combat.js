6e6ac292e0147fd394990454fbc8ebbbd1823892
Jimmys <jimmysilverstein@proton.me>
2024-09-12 23:01:19 +0100

    Cause rapid animations during ejaculation for consistency with old logic

--- game/03-JavaScript/05-renderer/00-combat.js
+++ game/03-JavaScript/05-renderer/00-combat.js
@@ -72,7 +72,7 @@ class CombatSystem {
 	}
 
 	isRapid() {
-		return this.isVaginaPenetrated() || this.isAnusPenetrated() || this.isMouthPenetrated() || this.isPenisPenetrated();
+		return this.anyEjaculating() || this.isVaginaPenetrated() || this.isAnusPenetrated() || this.isMouthPenetrated() || this.isPenisPenetrated();
 	}
 
 	anyEjaculating() {
