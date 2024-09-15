03a081ea1b30fe2bae45ef84cbe79a681f07d265
Jimmys <jimmysilverstein@proton.me>
2024-09-15 21:00:19 +0100

    Improvements to the combat editor

--- game/03-JavaScript/05-renderer/30-canvasmodel-editor.js
+++ game/03-JavaScript/05-renderer/30-canvasmodel-editor.js
@@ -1,5 +1,5 @@
 // @ts-check
-/* globals MultiCanvasModel */
+/* globals MultiCanvasModel, Partial, CompositeLayerSpec */
 
 class CombatEditor {
 	static createCanvasCombatEditor() {
@@ -9,12 +9,12 @@ class CombatEditor {
 		para.classList.add("mytest");
 		para.append(
 			this.createButton("Refresh Combat", () => {
-				this.recompileCombatCanvas();
+				CombatEditor.recompileCombatCanvas();
 			})
 		);
 		para.append(
 			this.createButton("Refresh Animation", () => {
-				this.refreshCombatCanvas();
+				CombatEditor.refreshCombatCanvas();
 			})
 		);
 		fragment.append(para);
@@ -31,8 +31,8 @@ class CombatEditor {
 
 		const container = document.createElement("div");
 		container.classList.add("d-flex", "flex-column");
-		const layers = this.getModelLayers(key);
-		const items = layers.map(layer => {
+		const layers = CombatEditor.getModelLayers(key);
+		const items = layers.map(function (layer) {
 			const btn = document.createElement("button");
 			btn.textContent = layer.name || "Unknown";
 			if (layer.show !== true) {
@@ -40,12 +40,76 @@ class CombatEditor {
 			}
 			btn.addEventListener("click", ev => {
 				console.log("Layer", layer.name, "clicked.");
+				CombatEditor.layer = layer;
+				const dialog = document.getElementById("combatMainLayerDialog");
+				if (dialog && dialog instanceof HTMLDialogElement) {
+					dialog.replaceChildren(CombatEditor.createLayerDialogContent(btn));
+					dialog.showModal();
+				}
 			});
 			return btn;
 		});
 		items.forEach(item => container.append(item));
 		fragment.append(container);
 
+		return fragment;
+	}
+
+	/**
+	 * @param {HTMLElement} parent
+	 * @returns {DocumentFragment}
+	 */
+	static createLayerDialogContent(parent) {
+		const fragment = document.createDocumentFragment();
+		const title = document.createElement("span");
+		title.textContent = CombatEditor.layer?.name || "Unknown";
+		fragment.append(title);
+		const showEle = document.createElement("input");
+		showEle.type = "checkbox";
+		showEle.checked = !!CombatEditor.layer?.show;
+		showEle.addEventListener("change", ev => {
+			if (CombatEditor.layer == null) {
+				return;
+			}
+			if (!(ev.target instanceof HTMLInputElement)) {
+				return;
+			}
+			CombatEditor.layer.show = ev.target.checked;
+			if (CombatEditor.layer.show) {
+				parent.classList.remove("faded");
+			} else {
+				parent.classList.add("faded");
+			}
+			CombatEditor.refreshCombatCanvas();
+		});
+		fragment.append(showEle);
+		return fragment;
+	}
+
+	static createLayerDialog() {
+		return CombatEditor.createDialog("combatMainLayerDialog", null);
+	}
+
+	/**
+	 * @param {string} id
+	 * @param {DocumentFragment?} content
+	 * @returns {DocumentFragment}
+	 */
+	static createDialog(id, content) {
+		const fragment = document.createDocumentFragment();
+		const dialog = document.createElement("dialog");
+		dialog.id = id;
+		dialog.classList.add("p-0");
+		dialog.addEventListener("click", ev => {
+			if (ev.target !== dialog) {
+				return;
+			}
+			dialog.close();
+		});
+		if (content) {
+			dialog.append(content);
+		}
+		fragment.append(dialog);
 		return fragment;
 	}
 
@@ -87,14 +151,11 @@ class CombatEditor {
 		const model = T.multiCombatModels[key];
 		return model.layers || [];
 	}
-
-	static addLayerElements(layer) {
-		const fragment = document.createDocumentFragment();
-
-		return fragment;
-	}
 }
 window.CombatEditor = CombatEditor;
+
+/** @type {Partial<CompositeLayerSpec>?} */
+CombatEditor.layer = null;
 
 Macro.add("canvasCombatEditor", {
 	handler() {
@@ -106,6 +167,13 @@ Macro.add("canvasCombatEditor", {
 Macro.add("addLayerElements", {
 	handler() {
 		const fragment = CombatEditor.createLayerEditor("combatMain");
+		this.output.append(fragment);
+	},
+});
+
+Macro.add("addLayerDialog", {
+	handler() {
+		const fragment = CombatEditor.createLayerDialog();
 		this.output.append(fragment);
 	},
 });
