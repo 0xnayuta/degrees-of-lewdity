580dc8f6f32c36b0a14cf8b17259e2c236346234
Vrelnir <vrelnir@gmail.com>
2025-01-08 23:19:39 +0000

    Updated to 0.5.3.5

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.3.4",
+	version: "0.5.3.5",
 	versionName: `"Houses Kept, Virginity Sold" edition`,
 	sneaky: false,
 	socialMediaEnabled: true,
