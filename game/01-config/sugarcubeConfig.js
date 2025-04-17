5c2ee3a08ce820ad0b94165457f49a6f75c79b54
Vrelnir <vrelnir@gmail.com>
2025-04-17 17:35:58 +0100

    Updated to 0.5.4.5

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.4.4",
+	version: "0.5.4.5",
 	versionName: `"Hopeless Cycle" edition`,
 	sneaky: false,
 	socialMediaEnabled: true,
