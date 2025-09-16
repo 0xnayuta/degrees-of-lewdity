5f46e10964fc5160fd453cedfb83952e1e455d8b
Vrelnir <vrelnir@gmail.com>
2025-09-16 15:28:16 +0100

    Updated to 0.5.5.7

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.5.6",
+	version: "0.5.5.7",
 	versionName: `"Pride Cometh" edition`,
 	sneaky: false,
 	socialMediaEnabled: true,
