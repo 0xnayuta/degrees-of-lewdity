5308de9978023f46c764457b3517462fbdf102b2
Vrelnir <vrelnir@gmail.com>
2025-11-02 00:16:34 +0000

    Updated to 0.5.6.5

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.6.4",
+	version: "0.5.6.5",
 	versionName: `"Love Bites" edition`,
 	sneaky: false,
 	socialMediaEnabled: true,
