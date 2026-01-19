019006eadf1cb5c2255a05da0655d16efc52d75a
majou <42233-majou@users.noreply.gitgud.io>
2026-01-19 22:13:02 +0000

    quest markers!

---
+++ game/base-system/questmarkers.js
@@ -0,0 +1,241 @@
+const events = [
+	{
+		// event name for debugging
+		name: "school day",
+		// conditions for the reminder to show up
+		// must be specific enough to no longer trigger once the event is actually attended
+		condition() {
+			return Time.schoolDay && Object.keys(V.daily.school.attended).length < 5;
+		},
+		// arriving at any time during this hour brings no penalties to the event
+		starthour: 8,
+		// arriving at any time after this hour will no longer trigger the event
+		endhour: 14,
+		// bigger number = higher chance of it being pulled
+		priority: 5, // prioritize avoiding truancy
+		// text shown in twine markup while event is scheduled
+		text: "<<schoolday>>",
+		// text shown if conditions are still met, but it's past the time to attend
+		failuretext:
+			"You have missed <<print Object.keys(V.daily.school.attended).length === 4 ? 'a lesson' : 5 - Object.keys(V.daily.school.attended).length + ' lessons'>> today...",
+	},
+	{
+		// example of a lazy event that will still work
+		name: "tomorrow",
+		condition() {
+			return Time.isSchoolDay(Time.tomorrow) && (!Time.schoolDay || Time.hour > 14);
+		},
+		text: "You have school tomorrow.",
+	},
+	{
+		// fallback event when no others are available
+		name: "no school",
+		condition() {
+			// other school-related events have higher priority anyway
+			return true;
+		},
+		text: "There is no school today.",
+		priority: 0,
+	},
+	{
+		name: "avery date",
+		condition() {
+			return V.avery_mansion?.schedule !== "away" && V.averydate === 1 && V.averydatedone !== 1 && Time.weekDay === 7;
+		},
+		starthour: 20,
+		endhour: 20,
+		priority: 4,
+		text: "You have a date with Avery <<print $avery_mansion ? 'in the garage': 'on Domus Street'>> <<if Time.hour is 20>><span class='gold'>right now!</span><<else>>at <<ampm 20 00>>.<</if>>",
+		failuretext: "You have missed your date with Avery. <<nnpc_He 'Avery'>> will not be happy...",
+	},
+	{
+		name: "community service",
+		condition() {
+			return V.community_service >= 1 && V.community_service_done !== 1;
+		},
+		starthour: 6,
+		endhour: 20,
+		priority: 1,
+		text: "The police station on Barb Street expects you for community service.",
+		failuretext: "You didn't attend your community service today...",
+	},
+	{
+		name: "harper appointment",
+		condition() {
+			return (V.harper_appointments.enabled || V.schoolPsych === 1) && Time.weekDay === 6 && V.daily.harperVisit !== 1;
+		},
+		starthour: 0,
+		endhour: 24,
+		priority: 1,
+		text: "You <<print $harper_appointments.enabled ? 'have an' : 'may get an'>> appointment with Doctor Harper at hospital on Nightingale Street.",
+		failuretext: "You have missed <<print $harper_appointments.enabled ? 'an' : 'a chance to get an'>> appointment with Doctor Harper...",
+	},
+	{
+		name: "brothel show",
+		condition() {
+			return V.brothelshowdata.type !== "none" && V.brothelshowdata.intro && Time.weekDay === 6;
+		},
+		starthour: 0,
+		endhour: 24,
+		priority: 1,
+		text: "You're expected to perform a <<print $brothelshowdata.type>> show at the brothel.",
+		failuretext: "You skipped the brothel show today...",
+	},
+	{
+		name: "escort job",
+		condition() {
+			const job = V.brothel_escortjob;
+			if (!job) return;
+			if (job.done || job.escape || job.missed || !job.accept) return;
+			return Time.monthDay === new DateTime(job.date).day;
+		},
+		get starthour() {
+			return new DateTime(V.brothel_escortjob.date).hour;
+		},
+		get endhour() {
+			return this.starthour;
+		},
+		priority: 6,
+		text: "You have an escort job at <<ampm `new DateTime(V.brothel_escortjob.date).hour`>>.",
+		failuretext: "You didn't attend your escort job...",
+	},
+	{
+		name: "wren heist",
+		condition() {
+			return V.wrenHeist === true && V.wrenHeistDance.attended !== true;
+		},
+		starthour: 19,
+		endhour: 20,
+		priority: 3,
+		text: "Wren waits for you between <<ampm 19 00>> and <<ampm 21 00>> on Danube Street.",
+	},
+	{
+		name: "smugglers",
+		condition() {
+			return V.smuggler_known === 1 && V.smuggler_timer === 0;
+		},
+		starthour: 21,
+		endhour: 24,
+		priority: 5,
+		text: "You heard that smugglers are moving their goods <<switch $smuggler_location>><<case 'forest'>>through the forest<<case 'sewer'>>through the old sewers<<case 'beach'>>at the rocks near the beach<<case 'bus'>>on a bus<<default>><</switch>> tonight, before midnight.",
+	},
+	{
+		name: "remy attacc",
+		condition() {
+			return V.farm_stage >= 7 && V.farm_attack_timer === 0;
+		},
+		starthour: 21,
+		endhour: 24,
+		priority: 3,
+		text: "Remy will attack the farm tonight between <<ampm 21 00>> and midnight.",
+	},
+	{
+		name: "adult shop help",
+		condition() {
+			return (
+				V.adultshopprogress < 22 &&
+				V.adultshopintro === 1 &&
+				V.adultshopunlocked === undefined &&
+				Time.weekDay === 6 &&
+				(Time.hour <= 15 || V.adultshopstate === "sydney")
+			);
+		},
+		starthour: 16,
+		endhour: 19,
+		priority: 6,
+		text: "Adult shop on Elk Street undergoes renovations between <<ampm 16>> and <<ampm 20>> today.",
+		failuretext: "You didn't help <<if C.npc.Sydney.init>>Sydney<</if>> at the adult shop today...",
+	},
+	{
+		name: "science fair",
+		condition() {
+			return V.scienceproject === "ongoing" && V.scienceprojectdays === 0;
+		},
+		starthour: 9,
+		endhour: 18,
+		priority: 5,
+		text: "The science fair is being held today on Cliff Street from <<ampm 9 00>> until <<ampm 18 00>>",
+		failuretext: "The science fair has ended. You didn't attend...",
+	},
+	{
+		name: "maths competition",
+		condition() {
+			return V.mathsprojectdays === 0 && V.mathsproject === "ongoing";
+		},
+		starthour: 9,
+		endhour: 18,
+		priority: 5,
+		text: "The maths competition is being held today on Cliff Street from <<ampm 9 00>> until <<ampm 18 00>>",
+		failuretext: "The maths competition has ended. You didn't attend...",
+	},
+	{
+		name: "english play",
+		condition() {
+			return V.englishPlayDays === 0 && V.englishPlay === "ongoing";
+		},
+		starthour: 17,
+		endhour: 20,
+		priority: 5,
+		text: "The school plays are being held today on Cliff Street from <<ampm 17 00>> until <<ampm 21 00>>",
+		failuretext: "The last of school plays is finished by now. You didn't attend...",
+	},
+];
+setup.events = events;
+
+Macro.add("questmarker", {
+	handler() {
+		const br = () => this.output.append(document.createElement("br"));
+		// silence notifications in hopeless cycle and bad ends
+		if (V.hc || ["prison", "asylum"].includes(V.location) || V.statFreeze) return;
+		const qualifiedEvents = events.filter(ev => ev.condition());
+		// display failure messages for missed events
+		qualifiedEvents.forEach(ev => {
+			if (!ev.failuretext || Time.hour <= (ev.endhour || 24)) return;
+			const failvar = ev.name + "fail";
+			if (V.hourly[failvar]) return;
+			V.hourly[failvar] = 1;
+			const div = document.createElement("div");
+			div.classList.add("purple");
+			div.append(Wikifier.wikifyEval(ev.failuretext));
+			this.output.append(div);
+			br();
+		});
+
+		let importants = [];
+		// find the most important notification
+		// first go events about to expire
+		importants = qualifiedEvents.filter(ev => ev.endhour === Time.hour);
+		// second go events underway
+		if (!importants.length) importants = qualifiedEvents.filter(ev => between(Time.hour, ev.starthour || 0, ev.endhour || 24));
+		// third go upcoming events
+		if (!importants.length) importants = qualifiedEvents.filter(ev => Time.hour < ev.starthour);
+		if (!importants.length) return;
+		// find the highest priority of available events
+		let topprio = 0;
+		importants.forEach(ev => {
+			// make sure priority exists for filtering later
+			ev.priority ||= 1;
+			if (ev.priority > topprio) topprio = ev.priority;
+		});
+		// pick a random event from highest priority ones
+		const pick = importants.filter(ev => ev.priority === topprio).random();
+		const div = document.createElement("div");
+		div.append(Wikifier.wikifyEval(pick.text));
+		this.output.append(div);
+		br();
+
+		/* alternatively, here goes the code to display all active events at once rather than the top one. can end up cluttering the sidebar.
+		couner = 0;
+		qualifiedEvents.forEach(ev => {
+			const div = document.createElement("div");
+			if (Time.hour <= (ev.endhour || 24)) {
+				div.append(Wikifier.wikifyEval(ev.text));
+				this.output.append(div);
+				br();
+				counter++;
+			}
+		});
+		if (counter > 0) this.output.append(br);
+		*/
+	},
+});
