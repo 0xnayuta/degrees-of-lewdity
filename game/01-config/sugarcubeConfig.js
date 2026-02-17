da8b69cb794af22df5d47e3c90ba31c39723eade
Vrelnir <vrelnir@gmail.com>
2026-02-17 01:11:26 +0000

    Updated to 0.5.8.7

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.8.6",
+	version: "0.5.8.7",
 	versionName: `"Assorted Chocolates" edition`,
 	sneaky: false,
 	socialMediaEnabled: true,
