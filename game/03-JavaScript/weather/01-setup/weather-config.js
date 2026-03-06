4291f8a84ba1e890bcb69c659fbbb15ae949e323
annon173 <email>
2026-03-06 13:33:25 -0500

    Changed lightningFrequency of heavyPrecipitation to once every 10 real minutes, on average

--- game/03-JavaScript/weather/01-setup/weather-config.js
+++ game/03-JavaScript/weather/01-setup/weather-config.js
@@ -342,7 +342,7 @@ setup.WeatherGeneration = {
 				fog: 0.3,
 				location: 0.2,
 			},
-			lightningFrequency: 0.2, // Average number of lightning strikes per minute
+			lightningFrequency: 0.1, // Average number of lightning strikes per minute
 			fogChangeRate: -0.01,
 		},
 		{
