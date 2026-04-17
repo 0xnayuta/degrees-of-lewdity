56cd65c236a2dd287214be3d541ebd84f498e312
Vrelnir <vrelnir@gmail.com>
2026-04-17 02:18:26 +0100

    Updated to 0.5.9.3

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.9.2",
+	version: "0.5.9.3",
 	versionName: `"Here Comes the Sun" edition`,
 	sneaky: true,
 	socialMediaEnabled: true,
