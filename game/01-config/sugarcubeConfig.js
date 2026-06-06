b47ca173e5b860c8d643169c632682b75cb5250b
Vrelnir <vrelnir@gmail.com>
2026-06-06 20:53:57 +0100

    Updated to 0.5.10.7

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.10.6",
+	version: "0.5.10.7",
 	versionName: `"Lofts of Clothes" edition`,
 	sneaky: false,
 	socialMediaEnabled: true,
