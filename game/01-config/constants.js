d18daa5f6c8bed12d88f4934f81fdc09e8b0266c
Yiphen <yiphenandsomeextrawords@gmail.com>
2026-06-20 05:26:28 -0700

    Player Stat and Fame Centralization Added constants to the "game\01-config\constants.js" file to centralize the game's existing minimum and maximum values for the player's stats and fame.
    Updated the "types\globals.d.ts" to include types for the new constant
    values in "game\01-config\constants.js".
    
    Will begin replacing the magic numbers based on these minimum and
    maximum values with the new constants in the following commit(s).

--- game/01-config/constants.js
+++ game/01-config/constants.js
@@ -21,6 +21,361 @@ const constants = {
 		hoursToLoseMinutes: 2,
 		hoursToLoseTime: 8,
 	},
+
+	/**
+	 * Minimum and maximum values for the player's stats
+	 * 
+	 * Stored in alphabetical order
+	 */
+
+	state: {
+		/**
+		 * Variable name: "drunk"
+		 */
+		alcohol: {
+			max: 1000,
+			min: 0,
+		},
+		arousal: {
+			max: 10000,
+			min: 0,
+		},
+		control: {
+			max: 1000,
+			min: 0,
+		},
+		/**
+		 * Variable name: "drugged"
+		 */
+		drugs: {
+			max: 1000,
+			min: 0,
+		},
+		/**
+		 * Variable name: "tiredness"
+		 */
+		fatigue: {
+			max: 2000,
+			min: 0,
+		},
+		/**
+		 * Variable name: "hallucinogen"
+		 */
+		hallucinogens: {
+			max: 1000,
+			min: 0,
+		},
+		pain: {
+			max: 200,
+			min: 0,
+		},
+		stress: {
+			max: 10000,
+			min: 0,
+		},
+		trauma: {
+			max: 5000,
+			min: 0,
+		},
+	},
+
+	characteristics: {
+		core: {
+			awareness: {
+				max: 1000,
+				min: -200,
+			},
+			beauty: {
+				max: 10000,
+				min: 0,
+			},
+			deviancy: {
+				max: 100,
+				min: 0,
+			},
+			exhibitionism: {
+				max: 100,
+				min: 0,
+			},
+			/**
+			 * The player's body can currently be 1 of 4 sizes:
+			 * 
+			 * Tiny, Small, Normal, and Large
+			 */
+			physique: {
+				absoluteMax: 20000,
+				largeMax: 15000,
+				normalMax: 12000,
+				smallMax: 9000,
+				tinyMax: 6000,
+				min: 0,
+			},
+			promiscuity: {
+				max: 100,
+				min: 0,
+			},
+			purity: {
+				min: 0,
+				nonVirginMax: 999,
+				virginMax: 1000,
+			},
+			willpower: {
+				max: 1000,
+				min: 0,
+			},
+		},
+		secondary: {
+			/**
+			 * Variable name: "submissive"
+			 */
+			grace: {
+				max: 100,
+				min: 0,
+			},
+			masochism: {
+				max: 1000,
+				min: 0,
+			},
+			sadism: {
+				max: 1000,
+				min: 0,
+			},
+			submissiveness: {
+				max: 2000,
+				min: 0,
+			},
+		},
+		fluids: {
+			milkVolume: {
+				cowMax: 6000,
+				normalMax: 3000,
+			},
+			semenVolume: {
+				cowMax: 6000,
+				normalMax: 3000,
+			},
+			/**
+			 * It seems there's no upper limit on the amount of fluid a vagina can produce when it orgasms. May be added in
+			 * a future update.
+			 * 
+			 * See "game\base-system\orgasm.twee" for more details.
+			 */
+		},
+	},
+
+	hairLength: {
+		max: 1000,
+		min: 0,
+	},
+	
+	fringeLength: {
+		max: 1000,
+		min: 0,
+	},
+
+	skills: {
+		core: {
+			athletics: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "danceskill"
+			 */
+			dance: {
+				max: 1000,
+				min: 0,
+			},
+			housekeeping: {
+				max: 1000,
+				min: 0,
+			},
+			skulduggery: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "swimmingskill"
+			 */
+			swimming: {
+				max: 1000,
+				min: 0,
+			},
+			tending: {
+				max: 1000,
+				min: 0,
+			},
+		},
+		school: {
+			english: {
+				max: 1000,
+				min: 0,
+			},
+			history: {
+				max: 1000,
+				min: 0,
+			},
+			maths: {
+				max: 1000,
+				min: 0,
+			},
+			science: {
+				max: 1000,
+				min: 0,
+			},
+		},
+		sex: {
+			/**
+			 * Variable name: "analskill"
+			 */
+			anal: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "bottomskill"
+			 */
+			buttocks: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "chestskill"
+			 */
+			chest: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "feetskill"
+			 */
+			feet: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "handskill"
+			 */
+			hands: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "oralskill"
+			 */
+			oral: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "seductionskill"
+			 */
+			seduction: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "thighskill"
+			 */
+			thighs: {
+				max: 1000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "vaginalskill"
+			 */
+			vaginal: {
+				max: 1000,
+				min: 0,
+			},
+		},
+	},
+
+	fame: {
+		negative: {
+			bestiality: {
+				max: 2000,
+				min: 0,
+			},
+			exhibitionism: {
+				max: 2000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "impreg"
+			 */
+			impregnation: {
+				max: 2000,
+				min: 0,
+			},
+			pimp: {
+				max: 2000,
+				min: 0,
+			},
+			pregnancy: {
+				max: 2000,
+				min: 0,
+			},
+			prostitution: {
+				max: 2000,
+				min: 0,
+			},
+			rape: {
+				max: 2000,
+				min: 0,
+			},
+			sex: {
+				max: 2000,
+				min: 0,
+			},
+		},
+		positive: {
+			business: {
+				max: 2000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "scrap"
+			 */
+			combat: {
+				max: 2000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "good"
+			 */
+			kindness: {
+				max: 2000,
+				min: 0,
+			},
+			model: {
+				max: 2000,
+				min: 0,
+			},
+			/**
+			 * Variable name: "social"
+			 */
+			socialite: {
+				max: 2000,
+				min: 0,
+			},
+		},
+	},
+	
+	/**
+	 * Original values obtained from "game\03-JavaScript\alias2.js"
+	 */
+	crime: {
+		max: 10000,
+		min: 0,
+		/**
+		 * If the player commits too much of the same type of crime in one day, they leave behind more evidence.
+		 * 
+		 * The dawnCheck() function in "game\03-JavaScript\time.js" will increase the player's crime by an additional
+		 * 10% if their daily crime stat is creater than "spree".
+		 */
+		spree: 1000,
+	},
 };
 
 /* Hoist Constants to the top (For statevars.js) */
