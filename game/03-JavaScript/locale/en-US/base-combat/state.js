1959855dd293afab61be3354c4b21e1a5e84b674
AsnDen <asnden@gmail.com>
2026-07-30 22:31:09 +0300

    feat: some more combat translations

--- game/03-JavaScript/locale/en-US/base-combat/state.js
+++ game/03-JavaScript/locale/en-US/base-combat/state.js
@@ -1,4 +1,11 @@
 Object.assign(setup.i18n["en-US"], {
+	"base-combat-state-look": "look",
+	"base-combat-state-looks": "looks",
+	"base-combat-state-does-not-look": "doesn't look",
+	"base-combat-state-do-not-look": "don't look",
+	"base-combat-state-is": "is",
+	"base-combat-state-are": "are",
+
 	"base-combat-state-anger-pissed-of": "<<He>> _is incredibly pissed off.",
 	"base-combat-state-anger-furious": "<<He>> _is furious.",
 	"base-combat-state-anger-angry": "<<He>> _looks angry.",
--- game/03-JavaScript/locale/ru-RU/base-combat/state.js
+++ game/03-JavaScript/locale/ru-RU/base-combat/state.js
@@ -5,11 +5,19 @@
  * For men it uses "разочарован" and "разочарована" for women.
  * See additional "a" at the end of the world. */
 Object.assign(setup.i18n["ru-RU"], {
-	"base-combat-state-anger-pissed-of": "<<He>> выглядит невероятно разозленным.",
-	"base-combat-state-anger-furious": "<<He>> выглядит яростно.",
-	"base-combat-state-anger-angry": "<<He>> выглядит злобно.",
-	"base-combat-state-anger-frustrated": "<<He>> выглядит разочарованно.",
-	"base-combat-state-anger-irritated": "<<He>> выглядит раздражённо.",
-	"base-combat-state-anger-tense": "<<He>> выглядит напражённо.",
-	"base-combat-state-anger-calm": "<<He>> выглядит спокойно.",
+	"base-combat-state-look": "выглядят",
+	"base-combat-state-looks": "выглядит",
+	"base-combat-state-does-not-look": "не выглядит",
+	"base-combat-state-do-not-look": "не выглядят",
+	// Those are empty cause of Russian grammar (not used).
+	"base-combat-state-is": "",
+	"base-combat-state-are": "",
+
+	"base-combat-state-anger-pissed-of": "<<He>> в бешенстве.",
+	"base-combat-state-anger-furious": "<<He>> _looks яростно.",
+	"base-combat-state-anger-angry": "<<He>> _looks злобно.",
+	"base-combat-state-anger-frustrated": "<<He>> _looks разочарованно.",
+	"base-combat-state-anger-irritated": "<<He>> _looks раздражённо.",
+	"base-combat-state-anger-tense": "<<He>> _looks напражённо.",
+	"base-combat-state-anger-calm": "<<He>> _looks спокойно.",
 });
