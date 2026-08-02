dd65a0f98249c5454055556dd1ac22a931c470ba
AsnDen <asnden@gmail.com>
2026-08-02 19:07:26 +0300

    fix: clean old locales for demonstation

--- game/03-JavaScript/locale/en-US/base-system/text.js
+++
@@ -1,7 +0,0 @@
-Object.assign(setup.i18n["en-US"], {
-	"base-system-text-pronouns-he": "he",
-	"base-system-text-pronouns-she": "she",
-	"base-system-text-pronouns-it": "it",
-	"base-system-text-pronouns-one": "one",
-	"base-system-text-pronouns-they": "they",
-});
