938b6f466350cd1d98781746189519f38365ff69
Vrelnir <vrelnir@gmail.com>
2026-08-24 22:21:56 +0100

    Updated to 0.5.12.2

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.12.1",
+	version: "0.5.12.2",
 	versionName: `"Name Pending" edition`,
 	sneaky: true,
 	socialMediaEnabled: true,
