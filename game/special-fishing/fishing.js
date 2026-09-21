03e9b813ec07737e725b773a938744a68bcc2742
Annon <email>
2026-09-21 12:19:18 -0400

    Added fishing wait text to help show the player when a fish is in season and when you're in it's preferred location

--- game/special-fishing/fishing.js
+++ game/special-fishing/fishing.js
@@ -25,7 +25,7 @@ function rollFishSize(bus, fishKey) {
 
 	// Baitfish are hard to come by, so making the fish that require them larger is nice for people trying to find max size of all fish
 	if (fishConfig.requiresBaitFish) {
-		preferredMatchCount += .75;
+		preferredMatchCount += 0.75;
 	}
 
 	const targetMean = lerp(Math.min(preferredMatchCount, 3) / 3, 0.45, 0.75);
@@ -196,17 +196,32 @@ function rollFish(bus) {
 window.rollFish = rollFish;
 
 /**
- * Returns the fish found at the location as [fishKey, weight] pairs, weighted by location rarity, for use with weightedRandom.
+ * Returns fish at the location whose preferred location includes the given location,
+ * as [fishKey, weight] pairs for use with weightedRandom.
  *
  * @param {string} location
  * @returns {Array}
  */
-function fishingSurfacingFish(location) {
+function preferredLocationFishList(location) {
 	return Object.entries(setup.fishing.lootTables.fish)
-		.filter(([, fishConfig]) => fishConfig.locations[location] > 0)
+		.filter(([, fishConfig]) => fishConfig.preferredLocation.includes(location))
 		.map(([fishKey, fishConfig]) => [fishKey, fishConfig.locations[location]]);
 }
-window.fishingSurfacingFish = fishingSurfacingFish;
+window.preferredLocationFishList = preferredLocationFishList;
+
+/**
+ * Returns fish at the location whose preferred season includes the current season,
+ * as [fishKey, weight] pairs for use with weightedRandom.
+ *
+ * @param {string} location
+ * @returns {Array}
+ */
+function preferredSeasonFishList(location) {
+	return Object.entries(setup.fishing.lootTables.fish)
+		.filter(([, fishConfig]) => fishConfig.locations[location] > 0 && fishConfig.preferredSeason.includes(Time.season))
+		.map(([fishKey, fishConfig]) => [fishKey, fishConfig.locations[location]]);
+}
+window.preferredSeasonFishList = preferredSeasonFishList;
 
 /**
  * If a fish you just hooked should be caught with the minigame.
